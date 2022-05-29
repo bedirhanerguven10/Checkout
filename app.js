fb1dba915194e7fd1bbb549bea376c2f1a131d95
Bedirhan <bedirhan.erguven@outlook.com>
2022-05-29 14:05:36 +0300

    new updates were added

--- app.js
+++ app.js
@@ -12,14 +12,19 @@
 let plus = document.querySelectorAll('.fa-plus');
 let minus = document.querySelectorAll('.fa-minus');
 let payment = document.querySelector('.payment');
+let remove = document.querySelectorAll('.remove')
 
 
 plus.forEach((item) => {
- item.addEventListener('click', add)
+    item.addEventListener('click', add)
 })
 
 minus.forEach((item) => {
     item.addEventListener('click', subtract)
+})
+
+remove.forEach((item) => {
+    item.addEventListener('click', removeitem)
 })
 
 
@@ -31,13 +36,33 @@ function add(item) {
 
    let price = this.parentElement.previousElementSibling.lastElementChild.firstElementChild.innerText;
    
-   payment.innerHTML = '$' + (parseFloat(price.slice(1))).toFixed(2) + (parseFloat(payment.innerHtml.slice(1)))
+   payment.innerHTML = '$' + (parseFloat(payment.innerHTML.slice(1)) + parseFloat(price.slice(1))).toFixed(2)
 }
 
-
+/* SUBTRACT */
 function subtract(item) {
     let amount = this.previousElementSibling;
-    if (amount.innerText > 1){
+
+    if (parseInt(amount.innerText) > 1){
     amount.innerText = parseInt(amount.innerText) -1;
+    let price = this.parentElement.previousElementSibling.lastElementChild.firstElementChild.innerText;
+
+    payment.innerHTML = '$' + ((parseFloat(payment.innerHTML.slice(1))) - (parseFloat(price.slice(1)))).toFixed(2)
 }
 }
+
+/* REMOVE */
+
+function removeitem(rem) {
+    let removePrice = parseFloat(this.previousElementSibling.previousElementSibling.lastElementChild.firstElementChild.innerHTML.slice(1));
+    let removeAmount = parseInt(this.previousElementSibling.firstElementChild.nextElementSibling.innerHTML);
+
+    payment.innerHTML = '$' + (parseFloat(payment.innerHTML.slice(1)) - (removePrice * removeAmount)).toFixed(2)
+
+    this.parentElement.parentElement.style.display = 'none';
+
+    if ((parseFloat(amount.innerHTML).slice(1)) <= 19 ) {
+    this.parentElement.parentElement.parentElement.parentElement.style.display = 'none'
+    }
+}
+
